6bc00b0bbc4858bcda0cab3e073c9c20c7bf9065
mshr-Hub <masa.megane.616@gmail.com>
2020-12-11 18:19:52 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    // this is JSX
-    // return <div>Hello, world!</div>;
+    // const greeting = 'Hi, Tom!'
+    // const dom = <h1 className="foo">{greeting}</h1>
+    // return dom;
 
-    // this is javascript
-    // return React.createElement(
-    //   "div",
-    //   null,
-    //   "Hell, world!!"
+    // return <input type="button" value="button" onClick={() => {console.log('I am clicked.')}}/>;
+    // return <input type="text" onChange={() => {console.log('I am changed.')}} />;
+
+    // 返せるタグは一括りのみで、複数のタグを返すことはできいない。
+    // return (
+    //   <div>
+    //     <label htmlFor="bar">bar</label>
+    //     <input type="text" onChange={() => {console.log('I am changed.')}} />
+    //   </div>
     // );
 
-    return <h1>Hello, world!</h1>;
+    // React.Flagment を使うと、余計なdivタグを出力させずに返すことができる。
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log('I am changed.')}} />
+      </React.Fragment>
+    );
   }
 }
 
