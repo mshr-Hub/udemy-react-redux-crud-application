b6237d859851a37844b1724df8b33ead872e9f6a
mshr-Hub <masa.megane.616@gmail.com>
2020-12-11 17:57:03 +0900

    change: jsx tag

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <div><h1>Hello, world!</h1></div>;
+    // this is JSX
+    // return <div>Hello, world!</div>;
+
+    // this is javascript
+    // return React.createElement(
+    //   "div",
+    //   null,
+    //   "Hell, world!!"
+    // );
+
+    return <h1>Hello, world!</h1>;
   }
 }
 
