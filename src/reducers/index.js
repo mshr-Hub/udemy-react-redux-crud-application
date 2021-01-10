d6319821e487a427247728b8fcbdc65ba1935f3b
mshr-Hub <masa.megane.616@gmail.com>
2021-01-10 18:32:58 +0900

    implement events index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
-import count from './count'
+import events from './events'
 
-export default combineReducers({ count })
+export default combineReducers({ events })
 
 // 状態を管理したい要素を追加する場合は、以下のように要素を追加する
 // export example combineReducers({ foo, bar, baz })
