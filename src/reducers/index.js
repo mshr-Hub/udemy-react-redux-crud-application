897331214aff1c201a5c17a05a5d876dc3f51679
mshr-Hub <masa.megane.616@gmail.com>
2021-01-24 20:33:03 +0900

    implement event new

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux'
+import { reducer as form } from 'redux-form'
 import events from './events'
 
-export default combineReducers({ events })
-
-// 状態を管理したい要素を追加する場合は、以下のように要素を追加する
-// export example combineReducers({ foo, bar, baz })
+// form --> form: form のシンタックスシュガー
+export default combineReducers({ events, form })
