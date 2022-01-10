396ec9765a41d87051f99713dfd385b991489d54
mshr-Hub <masa.megane.616@gmail.com>
2022-01-10 19:34:39 +0900

    implement eventsShow

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -37,12 +37,12 @@ class EventsShow extends Component {
   }
 
   async onSubmit(values) {
-    // await this.props.postEvent(values)
+    await this.props.putEvent(values)
     this.props.history.push('/')
   }
 
   render() {
-    const { handleSubmit, pristine, submitting } = this.props
+    const { handleSubmit, pristine, submitting, invalid } = this.props
 
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
@@ -50,7 +50,7 @@ class EventsShow extends Component {
         <div><Field label="Body" name="body" type="text" component={ this.renderField } /></div>
 
         <div>
-          <input type="submit" value="Submit" disabled={pristine || submitting} />
+          <input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
           <Link to="/">Cancel</Link>
           <Link to="/" onClick={this.onDeleteClick} >Delete</Link>
         </div>
@@ -73,7 +73,7 @@ const mapStateToProps = (state, ownProps) => {
   return { initialValues: event, event }
 }
 
-const mapDispatchToProps = ({ deleteEvent, getEvent })
+const mapDispatchToProps = ({ deleteEvent, getEvent, putEvent })
 
 export default connect(mapStateToProps, mapDispatchToProps)(
   reduxForm({ validate, form: 'eventShowFrom', enableReinitialize: true })(EventsShow)
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,9 +1,11 @@
 import _ from 'lodash'
-import { READ_EVENTS, READ_EVENT, DELETE_EVENT } from '../actions'
+import { CREATE_EVENT, READ_EVENTS, READ_EVENT, UPDATE_EVENT, DELETE_EVENT } from '../actions'
 
 export default (events = {}, action) => {
   switch (action.type) {
+    case CREATE_EVENT:
     case READ_EVENT:
+    case UPDATE_EVENT:
       const data = action.response.data
       return { ...events, [data.id]: data }
     case READ_EVENTS:
